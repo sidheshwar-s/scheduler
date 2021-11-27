f2b37aa505473db658df7133e756155378564b66
sidheshwar-s <sidheshwarsaravanan@gmail.com>
2021-11-27 13:32:43 +0530

    Feat: added scheduled notifcation

--- lib/addtask.dart
+++ lib/addtask.dart
@@ -4,6 +4,7 @@ import 'dart:ui';
 import 'package:intl/intl.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:scheduler_flutter/models/Time.dart';
 import 'home.dart';
 import 'package:date_time_picker/date_time_picker.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -28,9 +29,18 @@ class _addtaskState extends State<addtask> {
   DateFormat dateFormat = DateFormat("yyyy-MM-dd HH:mm");
   TextEditingController title = TextEditingController();
   TextEditingController description = TextEditingController();
-  static List time = ["1 hour", "45 mins", "30 mins", "15 mins"];
-  var _items =
-      time.map((time1) => MultiSelectItem<String>(time1, time1)).toList();
+  var selectedSchedules = [];
+  static List<TimeModel?> time = [
+    TimeModel(title: "1 hour", time: 60),
+    TimeModel(title: "45 mins", time: 45),
+    TimeModel(title: "30 mins", time: 30),
+    TimeModel(title: "15 min", time: 15),
+  ];
+  List<MultiSelectItem<TimeModel?>> _items = time
+      .map(
+        (curTime) => MultiSelectItem<TimeModel?>(curTime, curTime!.title),
+      )
+      .toList();
   DateTime datetime = DateTime.now();
   addtasktofirebase() async {
     try {
@@ -48,7 +58,13 @@ class _addtaskState extends State<addtask> {
         'time': datetime.toString(),
       });
       // Fluttertoast.showToast(msg: 'Data Added');
-      _showNotification(datetime, title.text);
+      selectedSchedules.forEach((value) {
+        print(value);
+        DateTime newTime = datetime.subtract(Duration(minutes: value.time));
+        print(newTime);
+        _showNotification(newTime, title.text, true, datetime);
+      });
+      _showNotification(datetime, title.text, false, datetime);
       Navigator.pop(context);
     } catch (e) {
       print(e);
@@ -70,7 +86,8 @@ class _addtaskState extends State<addtask> {
     );
   }
 
-  Future _showNotification(DateTime t, String title) async {
+  Future _showNotification(
+      DateTime t, String title, bool isScheduled, DateTime endTime) async {
     var androidDetails = new AndroidNotificationDetails(
         "Channel ID", "hello", "This is my channel",
         importance: Importance.max);
@@ -78,6 +95,17 @@ class _addtaskState extends State<addtask> {
     var generalNotificationDetails =
         new NotificationDetails(android: androidDetails, iOS: iSODetails);
     var scheduledTime = t;
+
+    if (isScheduled) {
+      fltrNotification.schedule(
+        1,
+        "Reminder!",
+        "your task with title ${title} will be over within ${endTime.difference(t).inMinutes} minutes",
+        scheduledTime,
+        generalNotificationDetails,
+      );
+      return;
+    }
     fltrNotification.schedule(
       1,
       "Reminder",
@@ -129,198 +157,195 @@ class _addtaskState extends State<addtask> {
         ],
       ),
       body: SingleChildScrollView(
-        child: Container(
-          height: MediaQuery.of(context).size.height,
-          width: MediaQuery.of(context).size.width,
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                SizedBox(height: 50),
-                Container(
-                  // color: Colors.blue,
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height / 3,
-                  decoration: BoxDecoration(
-                    // borderRadius: BorderRadius.circular(15.0),
-                    image: DecorationImage(
-                      image: AssetImage('assets/welcome5.png'),
-                      fit: BoxFit.fill,
-                      alignment: Alignment.center,
-                    ),
-                  ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(height: 20),
+            Container(
+              // color: Colors.blue,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height / 3,
+              decoration: BoxDecoration(
+                // borderRadius: BorderRadius.circular(15.0),
+                image: DecorationImage(
+                  image: AssetImage('assets/welcome5.png'),
+                  fit: BoxFit.fill,
+                  alignment: Alignment.center,
                 ),
-                Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                    // color: Colors.white,
-                  ),
-                  // color: Colors.white,
-                  height: 60,
-                  width: MediaQuery.of(context).size.width / 1.1,
-                  child: TextFormField(
-                    controller: title,
-                    keyboardType: TextInputType.text,
-                    textAlignVertical: TextAlignVertical.center,
-                    decoration: InputDecoration(
-                        hintText: "Enter the Title",
-                        border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10))),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 18.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10.0)),
-                      // color: Colors.white,
-                    ),
-                    // color: Colors.white,
-                    height: 60,
-                    width: MediaQuery.of(context).size.width / 1.1,
-                    child: TextFormField(
-                      controller: description,
-                      keyboardType: TextInputType.text,
-                      textAlignVertical: TextAlignVertical.center,
-                      decoration: InputDecoration(
-                          hintText: "Enter the Description",
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10))),
-                    ),
-                  ),
-                ),
-                // TextButton(
-                //     onPressed: () {
-                //       DatePicker.showDatePicker(context,
-                //           showTitleActions: true,
-                //           minTime: DateTime.now(),
-                //           maxTime: DateTime(2022, 6, 7), onChanged: (date) {
-                //         print('change $date');
-                //       }, onConfirm: (date) {
-                //         print('confirm $date');
-                //       }, currentTime: DateTime.now(), locale: LocaleType.en);
-                //     },
-                //     child: Text(
-                //       'show date time picker (Chinese)',
-                //       style: TextStyle(color: Colors.blue),
-                //     ))
-                const SizedBox(
-                  height: 10,
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 10),
-                  child: Align(
-                    alignment: Alignment.centerLeft,
-                    child: Text(
-                      "Pick your deadline :",
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 20.0, vertical: 10),
-                  child: DateTimePicker(
-                    // key: control,
-                    type: DateTimePickerType.dateTimeSeparate,
-                    dateMask: 'd MMM, yyyy',
-                    initialValue: DateTime.now().toString(),
-                    firstDate: DateTime(2000),
-                    lastDate: DateTime(2100),
-                    icon: Icon(Icons.event),
-                    dateLabelText: 'Date',
-                    timeLabelText: "Hour",
-                    // selectableDayPredicate: (DateTime date) {
-                    //   // Disable weekend days to select from the calendar
-                    //   if (date.weekday == 5 || date.weekday == 7) {
-                    //     return false;
-                    //   }
-
-                    //   return true;
-                    // },
-                    onChanged: (val) {
-                      print(val);
-                      print("**********");
-                      setState(() {
-                        DateTime dateTime = dateFormat.parse(val);
-                        print("hgvvu**********");
-                        print(dateTime);
-                        print("**********");
-                        datetime = dateTime;
-                      });
-                      DateTime dateTime = dateFormat.parse(val);
-                      print(dateTime);
-                      print(DateTime.now());
-                      print(dateTime.hour);
-                    },
-                    validator: (val) {
-                      print(val);
-                      return "please select a date";
-                    },
-                    onSaved: (val) {
-                      print(val);
-                      print("**********");
-                      DateTime dateTime =
-                          dateFormat.parse(val ?? "2019-07-19 8:40");
-                      print(dateTime);
-                    },
-                  ),
-                )
-                // Checkbox(value: value, onChanged: onChanged)
-                ,
-                Padding(
-                  padding: const EdgeInsets.only(top: 20.0),
-                  child: CheckboxListTile(
-                    activeColor: Colors.black,
-
-                    title: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: Text(
-                        "Do you want to schedule the task Regularly?",
-                        style: TextStyle(
-                            fontSize: 16, fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                    value: checkedValue,
-                    onChanged: (newValue) {
-                      setState(() {
-                        checkedValue = newValue;
-                      });
-                    },
-                    controlAffinity: ListTileControlAffinity
-                        .leading, //  <-- leading Checkbox
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 18.0),
-                  child: Container(
-                    width: MediaQuery.of(context).size.width / 1.1,
-                    child: MultiSelectChipField(
-                      items: _items,
-                      initialValue: [time[0], time[1], time[2], time[3]],
-                      title: Text("When Do you want reminders",
-                          style: TextStyle(
-                              fontSize: 16,
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white)),
-                      headerColor: Color(0xffFF6363),
-                      decoration: BoxDecoration(
-                        border:
-                            Border.all(color: Color(0xffFF6363), width: 1.8),
-                      ),
-                      selectedChipColor: Color(0xffFF6363),
-                      selectedTextStyle: TextStyle(
-                          color: Colors.white, fontWeight: FontWeight.bold
-                          // onTap: (values) {},
-                          ),
-                    ),
-                  ),
-                )
-              ],
+              ),
             ),
-          ),
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                // color: Colors.white,
+              ),
+              // color: Colors.white,
+              height: 60,
+              width: MediaQuery.of(context).size.width / 1.1,
+              child: TextFormField(
+                controller: title,
+                keyboardType: TextInputType.text,
+                textAlignVertical: TextAlignVertical.center,
+                decoration: InputDecoration(
+                    hintText: "Enter the Title",
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10))),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  // color: Colors.white,
+                ),
+                // color: Colors.white,
+                height: 60,
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: TextFormField(
+                  controller: description,
+                  keyboardType: TextInputType.text,
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                      hintText: "Enter the Description",
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                ),
+              ),
+            ),
+            // TextButton(
+            //     onPressed: () {
+            //       DatePicker.showDatePicker(context,
+            //           showTitleActions: true,
+            //           minTime: DateTime.now(),
+            //           maxTime: DateTime(2022, 6, 7), onChanged: (date) {
+            //         print('change $date');
+            //       }, onConfirm: (date) {
+            //         print('confirm $date');
+            //       }, currentTime: DateTime.now(), locale: LocaleType.en);
+            //     },
+            //     child: Text(
+            //       'show date time picker (Chinese)',
+            //       style: TextStyle(color: Colors.blue),
+            //     ))
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Pick your deadline :",
+                  style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10),
+              child: DateTimePicker(
+                // key: control,
+                type: DateTimePickerType.dateTimeSeparate,
+                dateMask: 'd MMM, yyyy',
+                initialValue: DateTime.now().toString(),
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+                icon: Icon(Icons.event),
+                dateLabelText: 'Date',
+                timeLabelText: "Hour",
+                // selectableDayPredicate: (DateTime date) {
+                //   // Disable weekend days to select from the calendar
+                //   if (date.weekday == 5 || date.weekday == 7) {
+                //     return false;
+                //   }
+
+                //   return true;
+                // },
+                onChanged: (val) {
+                  print(val);
+                  print("**********");
+                  setState(() {
+                    DateTime dateTime = dateFormat.parse(val);
+                    print("hgvvu**********");
+                    print(dateTime);
+                    print("**********");
+                    datetime = dateTime;
+                  });
+                  DateTime dateTime = dateFormat.parse(val);
+                  print(dateTime);
+                  print(DateTime.now());
+                  print(dateTime.hour);
+                },
+                validator: (val) {
+                  print(val);
+                  return "please select a date";
+                },
+                onSaved: (val) {
+                  print(val);
+                  print("**********");
+                  DateTime dateTime =
+                      dateFormat.parse(val ?? "2019-07-19 8:40");
+                  print(dateTime);
+                },
+              ),
+            )
+            // Checkbox(value: value, onChanged: onChanged)
+            ,
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: CheckboxListTile(
+                activeColor: Colors.black,
+
+                title: Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
+                  child: Text(
+                    "Do you want to schedule the task Regularly?",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                value: checkedValue,
+                onChanged: (newValue) {
+                  setState(() {
+                    checkedValue = newValue;
+                  });
+                },
+                controlAffinity:
+                    ListTileControlAffinity.leading, //  <-- leading Checkbox
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Container(
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: MultiSelectChipField(
+                  onTap: (values) {
+                    setState(() {
+                      selectedSchedules = values;
+                    });
+                  },
+                  items: _items,
+                  initialValue: [time[0], time[1], time[2], time[3]],
+                  title: Text("When Do you want reminders",
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  headerColor: Color(0xffFF6363),
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Color(0xffFF6363), width: 1.8),
+                  ),
+                  selectedChipColor: Color(0xffFF6363),
+                  selectedTextStyle: TextStyle(
+                      color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+          ],
         ),
       ),
     );
---
+++ lib/models/Time.dart
@@ -0,0 +1,8 @@
+class TimeModel {
+  final String title;
+  final int time;
+  TimeModel({
+    required this.title,
+    required this.time,
+  });
+}
